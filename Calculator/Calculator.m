b4db5062b0c30809ac02ca393dbb02d8d4b9d0b5
user <user@users-imac.local>
2016-08-24 15:38:04 +0900

    calculator 1

---
+++ Calculator/Calculator.m
@@ -0,0 +1,33 @@
+//
+//  Calculator.m
+//  sample
+//
+//  Created by user on 2016. 8. 23..
+//  Copyright © 2016년 ldcc. All rights reserved.
+//
+
+#import "Calculator.h"
+
+@implementation Calculator
+
+-(void) clear
+{
+    self.accumulator = 0;
+}
+-(void) add: (double) value
+{
+    self.accumulator += value;
+}
+-(void) subtract: (double) value
+{
+    self.accumulator -= value;
+}
+-(void) multiply: (double) value
+{
+    self.accumulator *= value;
+}
+-(void) divide: (double) value
+{
+    self.accumulator /= value;
+}
+@end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -7,16 +7,111 @@
 //
 
 #import "ViewController.h"
+#import "Calculator.h"
+
+enum opr {none, plus, minus, divide, multiply};
+typedef enum opr Operator;
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
 
 @end
 
 @implementation ViewController
+{
+    int currentValue;
+    Operator currentOpr;
+    Calculator *calc;
+    
+}
 
+
+- (IBAction)buttonClicked:(id)sender {
+    
+    UIButton *btn = (UIButton *)sender;
+    int value =(int)btn.tag;
+    
+    
+    if (currentOpr == none) {
+        currentValue = currentValue *10 + value;
+    } else  {
+        currentValue = value;
+        
+    }
+    
+    self.myLabel.text = [NSString stringWithFormat:@"%d",currentValue];
+
+}
+- (IBAction)clear:(id)sender {
+    currentValue =0;
+    self.myLabel.text=@"0";
+    
+    
+}
+- (IBAction)clickOpr:(id)sender {
+    
+    UIButton *btn = (UIButton *)sender;
+    int tagNum = (int)btn.tag;
+    
+    switch (tagNum) {
+        case 1:
+            currentOpr = plus;
+            break;
+            
+        case 2:
+            currentOpr = minus;
+            break;
+        case 3:
+            currentOpr = multiply;
+            break;
+        case 4:
+            currentOpr = divide;
+            break;
+            
+        default:
+            break;
+    }
+    
+    [calc setAccumulator:currentValue];
+    
+}
+- (IBAction)clickEqual:(id)sender {
+    switch (currentOpr) {
+        case plus:
+            [calc add:currentValue];
+            currentValue = [calc accumulator];
+            break;
+        case minus:
+            [calc subtract:currentValue];
+            currentValue = [calc accumulator];
+            break;
+        case multiply:
+            [calc multiply:currentValue];
+            currentValue = [calc accumulator];
+            break;
+        case divide:
+            [calc divide:currentValue];
+            currentValue = [calc accumulator];
+            break;
+        default:
+            break;
+    }
+    
+    self.myLabel.text=[NSString stringWithFormat:@"%d", currentValue];
+    
+}
+
+
+- (IBAction)unwind:(UIStoryboardSegue *)sender {
+    
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    calc = [[Calculator alloc] init];
+    currentOpr = none;
+    currentValue = 0;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,3 +120,16 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
